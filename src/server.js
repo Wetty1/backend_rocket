4b1c2ac8b268fda87c34ae777f2723aff782ca2a
Wesley de Sousa Silva <wettygamer@gmail.com>
2019-10-02 12:51:33 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+
+const app = express()
+mongoose.connect('mongodb+srv://user:user@cluster0-0v0b4.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+//req.query = Acessar query params
+//req.params = Acessar params
+//req.body = Acessar corpo da requisição
+
+app.use(express.json())
+app.use(routes)
+
+
+
+app.listen(3000);
+
+
